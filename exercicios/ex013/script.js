c44af0ab44cdaf513b7178bf40ecf805b3e9bbda
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-01 10:36:06 -0300

    ex13

--- exercicios/ex013/script.js
+++ exercicios/ex013/script.js
@@ -2,21 +2,26 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = 22
-    //var minuto = data.getMinutes()
-    //var segundo = data.getSeconds()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    //var segundos = data.getSeconds()
+    var relogio =data.toLocaleDateString()
     
     
-    msg.innerHTML = `Agora são ${hora}`
+    msg.innerHTML = `Hoje é ${relogio}  e ${hora}:${minutos} .`
+    //msg.innerHTML = `Agora são ${hora}:${minutos} :${segundos} horas`
 
 if (hora >= 0 && hora < 12) {
     //bom dia
     img.src = 'imagens/manha.png'
+    document.body.style.background = '#e2cd9f'
 } else if(hora >= 12 && hora < 18){
     //boa tarde
     img.src = 'imagens/tarde.png'
+    document.body.style.background = '#b9846f'
 } else {
     img.src = 'imagens/noite.png'
+    document.body.style.background = '#515154'
 
 }
 }
